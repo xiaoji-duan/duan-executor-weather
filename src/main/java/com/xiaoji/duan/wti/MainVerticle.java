0623bd891ac816624d0f975656d002c1fa53e181
leon_xi <leon_xi@163.com>
2019-06-17 15:35:56 +0800

    增加异常处理

--- src/main/java/com/xiaoji/duan/wti/MainVerticle.java
+++ src/main/java/com/xiaoji/duan/wti/MainVerticle.java
@@ -1,10 +1,17 @@
 package com.xiaoji.duan.wti;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.StringUtils;
 
 import io.vertx.amqpbridge.AmqpBridge;
 import io.vertx.amqpbridge.AmqpBridgeOptions;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.CompositeFuture;
 import io.vertx.core.Future;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.eventbus.Message;
@@ -58,10 +65,10 @@ public class MainVerticle extends AbstractVerticle {
 
 		JsonObject data = received.body().getJsonObject("body");
 
-		String locationid = data.getJsonObject("context").getString("locationid");
-		String type = data.getJsonObject("context").getString("type");
-		String date = data.getJsonObject("context").getString("date");
-		String time = data.getJsonObject("context").getString("time");
+		String locationid = data.getJsonObject("context").getString("locationid", "");
+		String type = data.getJsonObject("context").getString("type", "");
+		String date = data.getJsonObject("context").getString("date", "");
+		String time = data.getJsonObject("context").getString("time", "");
 		String next = data.getJsonObject("context").getString("next");
 
 		Date today = new Date();
@@ -77,8 +84,65 @@ public class MainVerticle extends AbstractVerticle {
 
 	}
 
-	private void weather(String consumer, String type, String locationid, String date, String time, String nextTask, Integer retry) {
+	private void getWeather1(Future<JsonObject> future, String locationid) {
+		String requesturi = config().getString("weather.uri.default", "http://www.weather.com.cn/data/sk/##locationid##.html").replaceAll("##locationid##", locationid);
+
+		client.getAbs(requesturi).send(handler -> {
+			if (handler.succeeded()) {
+				HttpResponse<Buffer> resp = handler.result();
+				
+				String jsonstring = resp.bodyAsString();
+				
+				if (jsonstring != null && jsonstring.trim().startsWith("{") && jsonstring.trim().endsWith("}")) {
+					JsonObject weather = new JsonObject(jsonstring);
+					
+					future.complete(weather);
+				} else {
+					future.complete(new JsonObject());
+				}
+				
+			} else {
+				future.fail(handler.cause());
+			}
+		});
+	}
+	
+	private void getWeather2(Future<JsonObject> future, String locationid) {
 		String requesturi = config().getString("weather.uri.cityinfo", "http://www.weather.com.cn/data/cityinfo/##locationid##.html").replaceAll("##locationid##", locationid);
+
+		client.getAbs(requesturi).send(handler -> {
+			if (handler.succeeded()) {
+				HttpResponse<Buffer> resp = handler.result();
+				
+				String jsonstring = resp.bodyAsString();
+				
+				if (jsonstring != null && jsonstring.trim().startsWith("{") && jsonstring.trim().endsWith("}")) {
+					JsonObject weather = new JsonObject(jsonstring);
+					
+					future.complete(weather);
+				} else {
+					future.complete(new JsonObject());
+				}
+				
+			} else {
+				future.fail(handler.cause());
+			}
+		});
+	}
+	
+	private void weather(String consumer, String type, String locationid, String date, String time, String nextTask, Integer retry) {
+		
+		// 非法输入参数正常返回
+		if (StringUtils.isEmpty(locationid) || StringUtils.isEmpty(type) || StringUtils.isEmpty(date) || StringUtils.isEmpty(time)) {
+			JsonObject nextctx = new JsonObject().put("context", new JsonObject().put("weather", new JsonObject()));
+			
+			MessageProducer<JsonObject> producer = bridge.createProducer(nextTask);
+			producer.send(new JsonObject().put("body", nextctx));
+			System.out.println(
+					"Consumer " + consumer + " send to [" + nextTask + "] result [" + nextctx.encode() + "]");
+			
+			return;
+		}
 		
 		mongodb.findOne("wti_location_weather", 
 				new JsonObject()
@@ -100,51 +164,30 @@ public class MainVerticle extends AbstractVerticle {
 					System.out.println(
 							"Consumer " + consumer + " send to [" + nextTask + "] result [" + nextctx.encode() + "]");
 				} else {
-					client.getAbs(requesturi).send(handler -> {
+					List<Future<JsonObject>> futures = new LinkedList<Future<JsonObject>>();
+					
+					Future<JsonObject> weather1Future = Future.future();
+					futures.add(weather1Future);
+					
+					getWeather1(weather1Future, locationid);
+					
+					Future<JsonObject> weather2Future = Future.future();
+					futures.add(weather2Future);
+					
+					getWeather2(weather2Future, locationid);
+					
+					CompositeFuture.all(Arrays.asList(futures.toArray(new Future[futures.size()])))
+					.map(v -> futures.stream().map(Future::result).collect(Collectors.toList()))
+					.setHandler(handler -> {
 						if (handler.succeeded()) {
-							HttpResponse<Buffer> resp = handler.result();
+							List<JsonObject> results = handler.result();
 							
-							String jsonstring = resp.bodyAsString();
+							JsonObject weather = new JsonObject();
 							
-							if (jsonstring != null && jsonstring.trim().startsWith("{") && jsonstring.trim().endsWith("}")) {
-								JsonObject weather = new JsonObject(jsonstring);
-								
-								weather.put("locationid", locationid);
-								weather.put("date", date);
-								weather.put("time", weather.getJsonObject("weatherinfo", new JsonObject()).getString("time", time));
-								weather.put("type", type);
-								
-								mongodb.save("wti_location_weather", weather, save -> {});
-								
-								JsonObject nextctx = new JsonObject().put("context", new JsonObject().put("weather", weather));
-								
-								MessageProducer<JsonObject> producer = bridge.createProducer(nextTask);
-								producer.send(new JsonObject().put("body", nextctx));
-								System.out.println(
-										"Consumer " + consumer + " send to [" + nextTask + "] result [" + nextctx.encode() + "]");
-
+							for (JsonObject result : results) {
+								weather.mergeIn(result);
 							}
 							
-						} else {
-							JsonObject nextctx = new JsonObject().put("context", new JsonObject().put("weather", new JsonObject()));
-							
-							MessageProducer<JsonObject> producer = bridge.createProducer(nextTask);
-							producer.send(new JsonObject().put("body", nextctx));
-							System.out.println(
-									"Consumer " + consumer + " send to [" + nextTask + "] result [" + nextctx.encode() + "]");
-						}
-					});
-				}
-			} else {
-				client.getAbs(requesturi).send(handler -> {
-					if (handler.succeeded()) {
-						HttpResponse<Buffer> resp = handler.result();
-						
-						String jsonstring = resp.bodyAsString();
-						
-						if (jsonstring != null && jsonstring.trim().startsWith("{") && jsonstring.trim().endsWith("}")) {
-							JsonObject weather = new JsonObject(jsonstring);
-
 							weather.put("locationid", locationid);
 							weather.put("date", date);
 							weather.put("time", weather.getJsonObject("weatherinfo", new JsonObject()).getString("time", time));
@@ -159,8 +202,55 @@ public class MainVerticle extends AbstractVerticle {
 							System.out.println(
 									"Consumer " + consumer + " send to [" + nextTask + "] result [" + nextctx.encode() + "]");
 
+						} else {
+							JsonObject nextctx = new JsonObject().put("context", new JsonObject().put("weather", new JsonObject()));
+							
+							MessageProducer<JsonObject> producer = bridge.createProducer(nextTask);
+							producer.send(new JsonObject().put("body", nextctx));
+							System.out.println(
+									"Consumer " + consumer + " send to [" + nextTask + "] result [" + nextctx.encode() + "]");
+						}
+					});
+				}
+			} else {
+				List<Future<JsonObject>> futures = new LinkedList<Future<JsonObject>>();
+				
+				Future<JsonObject> weather1Future = Future.future();
+				futures.add(weather1Future);
+				
+				getWeather1(weather1Future, locationid);
+				
+				Future<JsonObject> weather2Future = Future.future();
+				futures.add(weather2Future);
+				
+				getWeather2(weather2Future, locationid);
+				
+				CompositeFuture.all(Arrays.asList(futures.toArray(new Future[futures.size()])))
+				.map(v -> futures.stream().map(Future::result).collect(Collectors.toList()))
+				.setHandler(handler -> {
+					if (handler.succeeded()) {
+						List<JsonObject> results = handler.result();
+						
+						JsonObject weather = new JsonObject();
+						
+						for (JsonObject result : results) {
+							weather.mergeIn(result);
 						}
 						
+						weather.put("locationid", locationid);
+						weather.put("date", date);
+						weather.put("time", weather.getJsonObject("weatherinfo", new JsonObject()).getString("time", time));
+						weather.put("type", type);
+						
+						mongodb.save("wti_location_weather", weather, save -> {});
+						
+						JsonObject nextctx = new JsonObject().put("context", new JsonObject().put("weather", weather));
+						
+						MessageProducer<JsonObject> producer = bridge.createProducer(nextTask);
+						producer.send(new JsonObject().put("body", nextctx));
+						System.out.println(
+								"Consumer " + consumer + " send to [" + nextTask + "] result [" + nextctx.encode() + "]");
+
 					} else {
 						JsonObject nextctx = new JsonObject().put("context", new JsonObject().put("weather", new JsonObject()));
 						
